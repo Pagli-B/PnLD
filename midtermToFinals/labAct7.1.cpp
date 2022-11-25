cf691158f0593dd062f4291d7f152ecdf0edea2e
Pagli-B <81464213+Pagli-B@users.noreply.github.com>
2022-11-25 09:15:43 +0800

    Worked on and added the first activity in the laboratory activity 7.

---
+++ midtermToFinals/labAct7.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "N\tN^2\tN^3\tN^4";
+    int value = 1;
+    while (value <= 5)
+    {
+    cout << "\n" << value << "\t" << pow(value, 2) << "\t" << pow(value, 3) << "\t" << pow(value, 4); 
+    value++;
+    }
+}
